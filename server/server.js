6a0f185ea6e74cc44e492516333475ea59283ef6
AR3xAnish <anishsnilajkar@gmail.com>
2026-07-06 23:45:54 +0530

    mongodb connection

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/retrieva';
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const corsOptions = {
   origin: function (origin, callback) {
